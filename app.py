26848aecada2ddf63f51f130c55bd585aef3c4da
korawit <korawit.orkphol@gmail.com>
2024-02-29 03:45:42 +0000

    first version of react native backend

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
-
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+books=[
+    {"id":1,"title":"Harry Potter","author":"Author 1"},
+    {"id":2,"title":"ABC for kids","author":"Author 2"},
+    {"id":3,"title":"Book 3","author":"Author 3"}
+]
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    return "<p>Hello World</p>"
+
+@app.route("/books",methods=["GET"])
+@cross_origin()
+def get_all_books():
+    return jsonify(books)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000,debug=True)
